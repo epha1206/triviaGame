f1aa91fe7fe2f08c0d760d31ffcbb61eed03d92d
Elvis Pham <epha1206@gmail.com>
2018-02-28 15:36:36 -0500

    added if else statements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -52,11 +52,98 @@ $('#startOverBtn').on('click', function(){
 function newGame(){
     $('#finalMessage').empty();
     $('#correctAnswers').empty();
-    $('#incorrectAnswers').empyty();
+    $('#incorrectAnswers').empty();
     $('#unanswered').empty();
     currentQuestion = 0;
     correctAnswer = 0;
     incorrectAnswer = 0;
     unanswered = 0;
     newQuestion();
+}
+
+function newQuestion(){
+    $('#message').empty();
+    $('#correctedAnswer').empty();
+    answered = true;
+
+    $('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+triviaQuestions.length);
+    $('.question').html('<h2>' + triviaQuestions[currentQuestion].question + '</h2>');
+    for(var i = 0; i < 4; i++){
+        var choices = $('<div>');
+        choices.text(triviaQuestions[currentQuestion].answerList[i]);
+        choices.attr({'data-index': i});
+        choices.addClass('thisChoice');
+        $('.answerList').append(choices);
+    }
+
+    countdown();
+    $('.thisChoice').on('click', function(){
+        userSelect = $(this).data('index');
+        clearInterval(time);
+        answerPage();
+    });
+}
+
+function countdown(){
+    seconds: 15;
+    $('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+    answered = true;
+    time = setInterval(showCountdown, 1000);
+}
+
+function showCountdown(){
+    seconds--;
+    $('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+    if(seconds < 1){
+        clearInterval(time);
+        answered = false;
+        answerPage();
+    }
+}
+
+function answerPage(){
+    $('#currentQuestion').empty();
+    $('.thisChoice').empty();
+    $('.question').empty();
+    
+    var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+    var rightAnswerIndex = triviaQuestions[currentQuestion].answer;
+  
+    if((userSelect == rightAnswerIndex) && (answered == true)){
+        correctAnswer++;
+        $('#message').html(messages.correct);
+    }
+    else if((userSelect != rightAnswerIndex) && (answered == true)){
+        incorrectAnswer++;
+        $('#message').html(messages.incorrect);
+        $('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+    }
+    else{
+        unanswered++;
+        $('#message').html(messages.endTime);
+        $('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+        answered = true;
+    }
+
+    if(currentQuestion == (triviaQuestions.length-1)){
+        setTimeout(scoreboard, 5000)
+    }
+    else{
+        currentQuestion++;
+        setTimeout(newQuestion, 5000);
+    }
+}
+
+function scoreboard(){
+    $('#timeLeft').empty();
+    $('#message').empty();
+    $('#correctedAnswer').empty();
+
+    $('#finalMessage').html(messages.finished);
+    $('#correctAnswers').html("Correct Answers: " + correctAnswer);
+    $('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+    $('#unanswered').html("Unanswered: " + unanswered);
+    $('#startOverBtn').addClass('reset');
+    $('#startOverBtn').show();
+    $('#startOverBtn').html('Try Again?');
 }
